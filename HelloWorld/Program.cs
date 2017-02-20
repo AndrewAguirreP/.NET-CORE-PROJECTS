405dab84746c64229a4b66d88399857702091596
AndrewAguirreP <seesharpprojects@hotmail.com>
2017-02-20 14:31:29 +0800

    Project Setting / Hello Net Core 1.0

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+
+namespace ConsoleApplication
+{
+    public class Startup{
+        public void Configure(IApplicationBuilder app){
+            app.Run(
+                context => 
+                { 
+                    //Control log or Writeline
+                    return context.Response.WriteAsync("Hello drew on the web");
+                 }
+            );
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .Build();
+            
+            host.Run();
+            
+            /* Console.WriteLine("Hello World!");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name} welcome to .net core 1.0"); */
+           
+        }
+    }
+}
